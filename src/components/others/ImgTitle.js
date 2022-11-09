3c06f1387afdc7ba721f8a876be2c2ab7836e30e
goodnews <goodnewsj62@gmail.com>
2022-11-09 18:19:21 +0100

    detail article

--- src/components/others/ImgTitle.js
+++ src/components/others/ImgTitle.js
@@ -1,14 +1,15 @@
 import img from "static/images/test_img.jpg";
+import styles from "./styles/imgTitle.module.css";
 
 
-const ImgTitle = (props)=>{
+const ImgTitle = (props) => {
     return (
-        <div className="">
+        <div className={styles.img__div}>
             <img src={img} alt="featured img" />
-            <div className="title__div">
+            <div className={styles.title__div}>
                 <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis adipisci quae tenetur obcaecati molestias illum saepe quod dolore, totam alias magni porro iusto. Ducimus nostrum in soluta quam magnam tenetur?</h1>
-                <p className="mob__warning">
-                    click the arrow at the right side to see the list of content
+                <p className={styles.mobile__warning}>
+                    click the arrow at the right to see the list of content
                 </p>
             </div>
         </div>
