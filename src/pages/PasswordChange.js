c3ede2afe453450b47550769d5c91e6737993a8f
goodnews <goodnewsj62@gmail.com>
2023-01-25 15:01:31 +0100

    password change complete

--- src/pages/PasswordChange.js
+++ src/pages/PasswordChange.js
@@ -4,6 +4,7 @@ import PasswordField from "components/auth/authUtils/PasswordField";
 import Main from "components/others/MainWrapper";
 import { useState } from "react";
 import { useReducer } from "react";
+import { MdOutlineCancel } from "react-icons/md";
 import { useParams } from "react-router-dom";
 import { baseAxiosInstance } from "utils/requests";
 import styles from "./styles/pass.module.css";
@@ -32,22 +33,38 @@ const ChangePassword = (props) => {
     const handleSubmit =  async (event)=>{
         event.preventDefault();
         try{
-            const data =  {"token": params.token}
+            const data =  {"token": params.token, password: userInputs.password.content}
             const resp =  await baseAxiosInstance.post("auth/change-password/" , data);
             if (resp.status === 200) setMessage({type:"success", state:true});
         }catch(error){
-            setMessage({type:"failure", state:true})
+            setMessage({type:"failure", state:true});
         }
     };
 
+    const closMessageHandler =  ()=>{
+        setMessage({type:"", state:false});
+    }
+
+    const messageClasses =  message.type === "success"? styles.message : `${styles.message} ${styles.failure}`;
+
+
     return(
         <Main>
+            {
+                message.state && 
+                <div className={messageClasses}>
+                    <button onClick={closMessageHandler}>
+                        <MdOutlineCancel size={20} />
+                    </button>
+                    {message.type === "success"? <p>Password change successful</p> : <p>Oops an error occurred</p> }
+                </div>
+            }
             <form className={styles.form__cover} onSubmit={handleSubmit}>
                 <PasswordField dispatch={dispatch} fieldVal={userInputs.password} />
                 <ComfirmPasswordField dispatch={dispatch} fieldVal={userInputs.comfirmPass} passwordVal={userInputs.password} customClasses={customClasses} />
-                <button disabled={isValid}>
-                    {isValid}
-                </button>.toString()
+                <button disabled={!isValid}>
+                    Submit
+                </button>
             </form>
         </Main>
     )
