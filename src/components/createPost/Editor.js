09275b0d8ec926391fd086cbc11d173ab666d7b6
goodnews <goodnewsj62@gmail.com>
2023-01-30 23:48:31 +0100

    quill editor component

--- src/components/createPost/Editor.js
+++ src/components/createPost/Editor.js
@@ -1,19 +1,57 @@
 
 import { useRef } from "react";
-import { useEffect } from "react";
+
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import styles from "./styles/form.module.css";
+
 
 export default function Editor(props){
     const container =  useRef();
     const quillArea =  useRef();
 
-    useEffect(()=>{
-    }, [])
+    const modules= {
+        toolbar:[
+            [{ header: '1' }, { header: '2' }, { font: [] }],
+            [{ size: [] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [
+                { list: 'ordered' },
+                { list: 'bullet' },
+                { indent: '-1' },
+                { indent: '+1' },
+            ],
+            ['link', 'image', 'code-block'],
+            [{ 'script': 'sub'}, { 'script': 'super' }],
+
+            ['clean'],
+        ],
+        syntax:true,
+        clipboard: {
+          // toggle to add extra line breaks when pasting HTML:
+                matchVisual: false,
+        },
+    }
 
     return (
-        <section ref={container} className={styles.editor__container}>
-            <div id="toolbar"></div>
+        <section ref={container} id="quill__container" className={styles.editor__container}>
             <div ref={quillArea} className={styles.editor}  id="quill__editor">
+                <script>
+                    {
+                         window.hljs.configure({   // optionally configure hljs
+                            languages: ['javascript', 'ruby', 'python', "java", 'sql',
+                                            'yml','php','json', 'bash', 'css', 
+                                            'html', 'c#', 'c++']
+                        })
+                    }
+                </script>
+                <ReactQuill 
+                    placeholder="post content..." 
+                    theme="snow" 
+                    modules={modules}
+                    scrollingContainer="#quill__container"
+                    style={{width:"100%", height:"100%"}}
+                />
             </div>
         </section>
     )
