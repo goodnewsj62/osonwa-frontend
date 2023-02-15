16b869b3a5ff841809daba3bf19e3014717f42ea
goodnews <goodnewsj62@gmail.com>
2023-02-15 04:32:59 +0100

    likes page integration

--- src/pages/Liked.js
+++ src/pages/Liked.js
@@ -2,22 +2,61 @@ import ListCard from "components/others/cards/ListCard";
 import Main from "components/others/MainWrapper";
 import SearchLiked from "components/others/SearchLike";
 import ToggleContents from "components/others/ToggleContent";
+import useAuthAxios from "hooks/authAxios";
+import { useCallback, useEffect, useState } from "react";
 import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { articlePostListAdapter, newsListAdapter } from "utils/adapters";
 
 
 import cardStyles from "./styles/articles.module.css";
 import styles from "./styles/fav.module.css";
 
 const Liked = () => {
-    const fetchedArticles = [1, 2, 3].map((item) => {
-        return <ListCard />
+    const [fetchedArticles, setFetchedArticles] = useState({ isLoading: true, others: {}, posts: [] });
+    const [fetchedNews, setFetchedNews] = useState({ isLoading: true, others: {}, posts: [] });
+    const userInfo = useSelector((states) => states.profileState.userInfo)
+    const axios_ = useAuthAxios();
+
+
+    const fetchPost = useCallback(async (type) => {
+        try {
+            const url = `/liked/${userInfo.id}/?type=${type}`;
+            const resp = await axios_.get(url);
+            const { results, ...others } = resp.data.data;
+
+            if (type === "article") setFetchedArticles({ isLoading: false, others: others, posts: results });
+            else setFetchedNews({ isLoading: false, others: others, posts: results });
+
+            return resp;
+        } catch (err) {
+            if (type === "article") setFetchedArticles((state) => ({ ...state, isLoading: false }));
+            else setFetchedNews((state) => ({ ...state, isLoading: false }));
+        }
+
+    }, [axios_, userInfo.id]);
+
+    useEffect(() => {
+        Promise.all([fetchPost("article"), fetchPost("news")]);
+    }, [fetchPost]);
+
+
+
+    const articles = fetchedArticles.posts.map((item) => {
+        const info = articlePostListAdapter(item.content_object)
+        return <ListCard info={info} key={item.id} />
     });
 
-    const likedArticles = <section aria-label="liked article" className={cardStyles.articles}>{fetchedArticles}</section>;
-    const likedNews = <section aria-label="liked news" className={cardStyles.articles}>{[fetchedArticles[0]]}</section>;
+    const news = fetchedNews.posts.map((item) => {
+        const info = newsListAdapter(item.content_object)
+        return <ListCard info={info} key={item.id} />
+    });
+
+    const likedArticles = <section aria-label="liked article" className={cardStyles.articles}>{articles}</section>;
+    const likedNews = <section aria-label="liked news" className={cardStyles.articles}>{news}</section>;
 
     const contentNames = useMemo(() => ["news", "articles"], []);
-    const components = useMemo(() => [likedNews, likedArticles], []);
+    const components = [likedNews, likedArticles];
 
     const onScreen = () => { };
 
---
+++ src/utils/adapters.js
@@ -0,0 +1,53 @@
+
+
+export const newsListAdapter = (item) => {
+    const publisher_url = `source/news/${item.publisher}`;
+    const detail_url = `aggregate/${item.slug_title}-${item.post_id}`
+    const tagsInfo = { tags: item.tags, tagLink: "/news/tags/" };
+    const likeInfo = { count: item.likes, type: "news", likeUrl: `/liked/${item.id}/`, is_liked: item.is_liked };
+    const starInfo = { starUrl: `/saved/${item.id}/`, type: "news", saved: item.is_saved };
+
+
+    return {
+        detailUrl: detail_url,
+        imgSrc: item.image,
+        dpSrc: item.pub_image,
+        publisher: item.publisher,
+        publisherUrl: publisher_url,
+        date: item.date_published,
+        title: item.title,
+        content: item.content,
+        tagsInfo: tagsInfo,
+        likeInfo: likeInfo,
+        commentInfo: {},
+        starInfo: starInfo,
+        shareUrl: process.env.REACT_APP_DOMAIN + "/" + detail_url
+    }
+}
+
+
+export const articlePostListAdapter = (item) => {
+    const publisher = item.is_post ? item.publisher.first_name + " " + item.publisher.last_name : item.publisher
+    const publisher_url = item.is_post ? item.publisher.username : `source/article/${item.publisher}`;
+    const detail_url = item.is_post ? `article/${item.slug_title}-${item.post_id}` : `aggregate/${item.slug_title}-${item.post_id}`
+    const tagsInfo = { tags: item.tags, tagLink: item.is_post ? "/blog/tags/" : "/article/tags/" };
+    const likeInfo = { count: item.likes, type: item.is_post ? "post" : "article", likeUrl: `/liked/${item.id}/`, is_liked: item.is_liked };
+    const starInfo = { starUrl: `/saved/${item.id}/`, type: item.is_post ? "post" : "article", saved: item.is_saved };
+
+
+    return {
+        detailUrl: detail_url,
+        imgSrc: item.image,
+        dpSrc: item.pub_image,
+        publisher: publisher,
+        publisherUrl: publisher_url,
+        date: item.date_published,
+        title: item.title,
+        content: item.content,
+        tagsInfo: tagsInfo,
+        likeInfo: likeInfo,
+        commentInfo: {},
+        starInfo: starInfo,
+        shareUrl: process.env.REACT_APP_DOMAIN + "/" + detail_url
+    }
+}
